e7bba2c93b57073bb6bba1781d1af577eafe879d
Antti Leppä <antti.leppa@metatavu.fi>
2017-01-25 15:32:37 +0200

    Generated source

---
+++ java-client-generated/src/main/java/fi/metatavu/restfulptv/client/ApiClient.java
@@ -0,0 +1,13 @@
+package fi.metatavu.restfulptv.client;
+
+import java.util.Map;
+
+public interface ApiClient {
+  
+  public <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+}
